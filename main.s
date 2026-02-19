4bf661183f595732bb71b781d06719b84d0a6d4f
IC\pm823 <pablomarquezromeu@gmail.com>
2026-02-19 10:02:26 +0000

    no message

--- main.s
+++ main.s
@@ -1,5 +1,6 @@
 #include <xc.inc>
 
+global	myArray, counter
 extrn	UART_Setup, UART_Transmit_Message  ; external subroutines, tells the compiler it will have to access these routines from other files
 	
 psect	udata_acs   ; reserve data space in access ram
@@ -12,7 +13,7 @@ myArray:    ds 0x80 ; reserve 128 bytes for message data
 psect	data    
 	; ******* myTable, data in programme memory, and its length *****
 myTable:
-	db	'H','e','l','l','o',' ','W','o','r','l','d','!',0x0a
+	db	'H','e','l','l','o',' ','W','o','r','l','d','!',0x0d
 					; message, plus carriage return
 	myTable_l   EQU	13	; length of data
 	align	2
