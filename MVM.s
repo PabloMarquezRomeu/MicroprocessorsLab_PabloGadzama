15bf9064517db163993b588e488b75b50f25eab8
IC\pm823 <pablomarquezromeu@gmail.com>
2026-02-20 15:28:02 +0000

    Working MVM file that multiplies first row of matrix A with first row of matrix B

--- MVM.s
+++ MVM.s
@@ -1,10 +1,53 @@
-;This file contains a MVMLoop that will use the functions PMRAMCopy etc...
+#include <xc.inc>
+#include "tblptr_macros.inc"
+    ;This file contains a MVMLoop that will use the functions PMRAMCopy etc...
 
-MVMLOOP:
+extrn	columnB, rowA, runningSum, matrix_l, matrixA, counter
+global  MVMLoop  
+    
+psect	uart_code, class=CODE
+MVMLoop:
     ;For now only implement this loop once, then well loop it
     ;call PMRAMCopy
     ;call MACOperation
     ;call WriteY
     ;repeat this until all rows done
+   
+    movlw   matrix_l	; 3 bytes to read
+    movwf   counter, A	; our counter register
+    TBLPTR_POINT_TO matrixA
+    lfsr    0, rowA
+    call    PMRAMCopy	; copies the first row of matrix A onto RAM memory
+    
+    movlw   matrix_l	; Loads the number of bytes we have to multiply to our counter register for the MACloop
+    movwf   counter, A	
+    lfsr    0, rowA		; load row A onto FSR0
+    lfsr    1, columnB	; load columnB onto FSR1
+    call MACOperation
+    
+    return
+    
+    
+PMRAMCopy:
+    tblrd*+			; move one byte from PM to TABLAT, increment TBLPRT
+    movff   TABLAT, POSTINC0
+    decfsz  counter, A
+    bra	    PMRAMCopy
+    
+    return
+    
+    
+MACOperation:
+    movf    POSTINC0, W, A	; move FSR0 to working
+    mulwf   POSTINC1, A	; multiply workin with FSR1. 8bit multiplication in this case, so 16bit result
+    movf    PRODL, W, A	; Move low result to working and add it to low runnin sum byte
+    addwf   runningSum, F, A
+    movf    PRODH, W, A	; Move high result to working and add it to high running sum byte
+    addwfc  runningSum+1, F, A
+
+    decfsz  counter, A	; count down to zero
+    bra	    MACOperation	; keep going until finished
+    
+    return
 
 
--- main.s
+++ main.s
@@ -1,12 +1,14 @@
 #include <xc.inc>
-#include "tblptr_macros.inc"  ;This would allow us to write TBLPTR_POINT_TO rowA
+#include "tblptr_macros.inc"  
 
-
-global	columnB, rowA, runningSum
+    
+    
+extrn	MVMLoop	    ;Subroutines from other files
+global	columnB, rowA, runningSum, matrix_l, matrixA, counter
     
 psect	udata_acs
 matrix_count:	ds 1 ;reserve one byte in access ram
-counter:	ds 2
+counter:	ds 1
 runningSum:	ds 0x02 ;reserve 2 bytes in bank 4 in ram for the 16bit result of an 8 bit multiplication
     
 psect	udata_bank5
@@ -56,42 +58,23 @@ writeloop:
 
 	; ******* Main programme *********************
 start:	
-	;Point FSR0 to temporary matrixA row
-	;Point FSR1 to vectorX in RAM
-	;Length is defined as a global variable
-	;call MVMLoop
 	clrf	runningSum, A
 	clrf	runningSum+1, A
 	
-	TBLPTR_POINT_TO matrixA
 	movlw	matrix_l	; 3 bytes to read
 	movwf 	counter, A	; our counter register
-	lfsr	0, rowA
-	call	writeloop	; write from matrixA in PM to rowA in RAM
-	
 	TBLPTR_POINT_TO matrixB
-	movlw	matrix_l	; 3 bytes to read
-	movwf 	counter, A	; our counter register
 	lfsr	0, columnB	; load columnB onto FSR0
 	call	writeloop	; write from matrixB in PM to column B in RAM
 	
-	movlw	matrix_l	; 3 bytes to read
-	movwf 	counter, A	; our counter register for the MACloop
-	lfsr	0, rowA		; load row A onto FSR0
-	lfsr	1, columnB	; load columnB onto FSR1
-
 	
-MACloop:
-
-	movf	POSTINC0, W	; move FSR0 to working
-	mulwf	POSTINC1	; multiply workin with FSR1. 8bit multiplication in this case, so 16bit result
-	movf	PRODL, W, A	; Move low result to working nd add it to low runnin sum byte
-	addwf	runningSum, F, A
-	movf	PRODH, W, A	; Move high result to working and add it to high running sum byte
-	addwfc	runningSum+1, F, A
-
-	decfsz	counter, A	; count down to zero
-	bra	MACloop		; keep going until finished
-
-
-	end	rst
+	;Point FSR0 to temporary matrixA row
+	;Point FSR1 to vectorX in RAM
+	;Length is defined as a global variable
+	lfsr	0, rowA	; load columnB onto FSR0
+	lfsr	1, columnB	; load columnB onto FSR0
+	call	MVMLoop
+	nop
+	nop
+	end rst
+	
