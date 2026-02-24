3c958ec8716e05d3820556d58fc71f2cad46cd64
IC\pm823 <pablomarquezromeu@gmail.com>
2026-02-24 14:07:35 +0000

    Fully implemented MVM of length 3. Currently vector X, Y are both 8bit vectors (2 hex) and so i can only store the low bit of the runningSum in the y vector.

--- MVM.s
+++ MVM.s
@@ -2,29 +2,54 @@
 #include "tblptr_macros.inc"
     ;This file contains a MVMLoop that will use the functions PMRAMCopy etc...
 
-extrn	vectorX, rowA, runningSum, matrixA, counter
-global  MVMLoop  
+global  MVMLoop, matrixA, vectorX, vectorY, rowA, runningSum, counter, matrix_count
+
+    
+psect	udata_acs ;Reserves UNDEFINED data in access ram (start of ram)
+matrix_count:	ds 1 ;reserve one byte in access ram
+counter:	ds 1
+runningSum:	ds 0x02 ;reserve 2 bytes in ram for the 16bit result of an 8 bit multiplication
+
+psect	udata_bank5 ;Reserves UNDEFINED data specifically in bank 5 of RAM
+rowA:		ds matrix_l ;reserve 3 bytes in ram
+vectorX:	ds matrix_l 
+vectorY:	ds matrix_l ;reserve 3 bytes in RAM for vector Y
+    
+psect	data ;stores data in PM
+matrixA:
+    db	0x03, 0x02, 0x01
+    db	0x02, 0x01, 0x02
+    db	0x01, 0x02, 0x01
+    align   2
+
     
 psect	uart_code, class=CODE
 MVMLoop:
-    ;For now only implement this loop once, then well loop it
-    ;call WriteY
-    ;repeat this until all rows done
-   
+    TBLPTR_POINT_TO matrixA ;Points TBL to matrixA in PM memory
+    lfsr    2, vectorY ;Points FSR2 to vector Y
     movlw   matrix_l	; 3 bytes to read
     movwf   counter, A	; our counter register
-    TBLPTR_POINT_TO matrixA ;Points TBL to matrixA in PM memory
+    call    clearVariable ;Clear the entire vector Y
+    lfsr    2, vectorY ;Points FSR2 to vector Y
+    
+rowLoop:
+    clrf    runningSum, A ;Important to clear as when ram is initialised it will have random numbers
+    clrf    runningSum+1, A
+    
+    movlw   matrix_l	; 3 bytes to read
+    movwf   counter, A	; our counter register
     lfsr    0, rowA ; Points FSR0 to the data space in RAM for the temporary row from matrix A
     call    PMRAMCopy	; copies the first row of matrix A onto RAM memory
     
     movlw   matrix_l	; Loads the number of bytes we have to multiply to our counter register for the MACloop
     movwf   counter, A	
-    lfsr    0, rowA		; load row A onto FSR0
+    lfsr    0, rowA	; load row A onto FSR0
     lfsr    1, vectorX	; load columnB onto FSR1
-    call MACOperation ;Multiplies the numbers pointed by FSR0 and FSR1, and adds this to the runningsum. These two loop through rowA and vectorX
+    call    MACOperation ;Multiplies the numbers pointed by FSR0 and FSR1, and adds this to the runningsum. These two loop through rowA and vectorX
+    movff   runningSum, POSTINC2 ;Stores only the low byte for now in vector Y
     
-    ;call WriteY We will have to somehow implement something that writes the running sum to the correct position in vectorY
-    ;We will also need this to loop until all the vectorY is filled properly
+    decfsz  matrix_count, A	; count down to zero
+    bra	    rowLoop	; keep going until finished
     
     return
     
@@ -52,4 +77,9 @@ MACOperation:
     return
 
 
+clearVariable:
+    clrf    POSTINC2, A
+    decfsz  counter, A
+    bra	    clearVariable
 
+    return
--- main.s
+++ main.s
@@ -1,25 +1,8 @@
 #include <xc.inc>
 #include "tblptr_macros.inc"  
 
-extrn	MVMLoop	    ;Subroutines from other files
-global	vectorX, rowA, runningSum, matrixA, counter
+extrn	MVMLoop, matrixA, vectorX, vectorY, rowA, runningSum, counter, matrix_count	    ;Subroutines from other files
     
-psect	udata_acs ;Reserves UNDEFINED data in access ram (start of ram)
-matrix_count:	ds 1 ;reserve one byte in access ram
-counter:	ds 1
-runningSum:	ds 0x02 ;reserve 2 bytes in ram for the 16bit result of an 8 bit multiplication
-
-psect	udata_bank5 ;Reserves UNDEFINED data specifically in bank 5 of RAM
-rowA:		ds 0x03 ;reserve 3 bytes in ram
-vectorX:	ds 0x03 
-vectorY:	ds 0x03 ;reserve 3 bytes in RAM for vector Y
-
-psect	data ;stores data in PM
-matrixA:
-    db	0x03, 0x02, 0x01
-    db	0x02, 0x01, 0x02
-    db	0x01, 0x02, 0x01
-    align   2
 
 psect code, abs  ;eerything that follows this psect will be stored as executable code in PM. The abs means that we will literally choose where to store this.
 rst:	org 0x0
@@ -33,8 +16,6 @@ setup:
 
 	; ******* Main programme *********************
 start:	
-	clrf	runningSum, A ;Important to clear as when ram is initialised it will have random numbers
-	clrf	runningSum+1, A
 	movlb   5; Point to bank 5. I have to do this for now, but hopefully this is where the data from the sensor will be stored
 	movlw	0x01 
 	movwf	vectorX, B
@@ -43,11 +24,9 @@ start:
 	movlw	0x03
 	movwf	vectorX+2, B
 	
-
-	;Length is defined as a global variable is the tblptr macros file for now. It should be processed from data
-	lfsr	0, rowA	;Point FSR0 to temporary matrixA row
-	lfsr	1, vectorX ;Point FSR1 to vectorX in RAM
-	call	MVMLoop
+	movlw   matrix_l	; 3 bytes to read
+	movwf   matrix_count, A
+	call	MVMLoop ;This function will use FSR0, 1, 2 to multiply the matrix A stored in PM with the vector X stored in RAM, and will output the result in RAM
 	nop
 	nop
 	end rst
