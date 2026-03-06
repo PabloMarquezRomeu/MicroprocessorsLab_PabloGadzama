34b5702c4f1f95eac54aceb1af1c632162fc6c42
IC\mg2723 <melejgadzama@gmail.com>
2026-03-06 11:19:46 +0000

    changing the sinc table

--- sinc.s
+++ sinc.s
@@ -1,13 +1,12 @@
 #include <xc.inc>
-
-psect flash_data,class=CODE,space=0
-
-global myTable
-global myTable_len
-
-myTable:
+    
+global Sinc_x
+    
+psect 	data
+	
+Sinc_x:
     db 252,3,0, 6, 249, 5, 0 ,250, 12, 242, 11, 0, 230, 127,127
     db 230, 0, 11, 242, 12, 250, 0, 5, 249, 6, 253, 0 , 0, 252
     
-myTable_len equ 22
+    align 2
 
