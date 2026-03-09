ad2e39236d2374d97cbf882f6e5992710b2206cc
IC\pm823 <pablomarquezromeu@gmail.com>
2026-03-09 13:59:17 +0000

    Revert "no message"
    This reverts commit 4bf661183f595732bb71b781d06719b84d0a6d4f.

--- main.s
+++ main.s
@@ -1,6 +1,5 @@
 #include <xc.inc>
 
-global	myArray, counter
 extrn	UART_Setup, UART_Transmit_Message  ; external subroutines, tells the compiler it will have to access these routines from other files
 	
 psect	udata_acs   ; reserve data space in access ram
@@ -13,7 +12,7 @@ myArray:    ds 0x80 ; reserve 128 bytes for message data
 psect	data    
 	; ******* myTable, data in programme memory, and its length *****
 myTable:
-	db	'H','e','l','l','o',' ','W','o','r','l','d','!',0x0d
+	db	'H','e','l','l','o',' ','W','o','r','l','d','!',0x0a
 					; message, plus carriage return
 	myTable_l   EQU	13	; length of data
 	align	2
